3afa8f8711167e1bdcbe8a005c7f71bf20982e58
14doan <14doan@gmail.com>
2022-09-06 15:46:41 +0100

    new

--- pages/sentences/[id].js
+++ pages/sentences/[id].js
@@ -1,38 +1,41 @@
-export const getStaticPaths= async()=>{
-    const res= await fetch('http://localhost:8000/sentences');
-    const data= await res.json();
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    "https://github.com/14doan/thailearn/blob/main/data/db.json/"
+  );
+  const data = await res.json();
 
-    const paths= data.map(s=>{
-        return{
-            params:{id:s.id.toString()}
-        }
-    })
-
+  const paths = data.map((s) => {
     return {
-        paths,
-        fallback:false
-    }
-}
+      params: { id: s.id.toString() },
+    };
+  });
 
-export const getStaticProps = async(context) => {
-    const id = context.params.id;
-    const res = await fetch ('http://localhost:8000/sentences/' +id);
-    const data = await res.json();
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-    return{
-        props: {sentence:data}
-    }
-}
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(
+    "https://github.com/14doan/thailearn/blob/main/data/db.json/" + id
+  );
+  const data = await res.json();
 
+  return {
+    props: { sentence: data },
+  };
+};
 
-const Details = ({sentence}) => {
-    return ( 
-        <div>
-            <h2>{sentence.english}</h2>
-            <p>{sentence.thai}</p>
-            <p>{sentence.note}</p>
-        </div>
-     );
-}
- 
-export default Details ; 
+const Details = ({ sentence }) => {
+  return (
+    <div>
+      <h2>{sentence.english}</h2>
+      <p>{sentence.thai}</p>
+      <p>{sentence.note}</p>
+    </div>
+  );
+};
+
+export default Details;
