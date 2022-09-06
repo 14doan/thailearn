767eb5a2b92ab40f5d72fe30c99fb223ba49db50
14doan <14doan@gmail.com>
2022-09-06 15:21:30 +0100

    [router]

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,24 @@
 import Link from "next/link";
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, [router]);
 
-    const router=useRouter();
-    useEffect(()=>{
-        setTimeout(()=>{
-            router.push('/');
-        },5000)
-    },[])
+  return (
+    <div className="notFound">
+      <h2>Computer says no</h2>
+      <p>Blame the computer</p>
+      <p>
+        Going back to <Link href="/">Home</Link> in 5 seconds...
+      </p>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="notFound">
-            <h2>Computer says no</h2>
-            <p>Blame the computer</p>
-            <p>Going back to <Link href="/">Home</Link> in 5 seconds...</p>
-        </div>
-     );
-}
-    
 export default NotFound;
